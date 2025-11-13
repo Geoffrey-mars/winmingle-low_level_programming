8e5c1d172d64acc0e91fda4eb15180cb7dd0a992
Habila Geoffrey <makrogh120@gmail.com>
2025-11-12 19:29:32 -0800

    Add a Program that print the lowercase alphabet in reverse order.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+ /**
+ * Author: Your Full Name
+ * Program: WinMingle Community C Training
+ * Description: Brief description of this file
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+  char reverse = 'z';
+
+  while (reverse >= 'a')
+  {
+      putchar(reverse);
+      reverse --;
+  }
+
+  putchar('\n');
+  return (0);
+}
