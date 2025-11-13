1c21d361f997b4058195e792726bd3f450017ef6
Habila Geoffrey <makrogh120@gmail.com>
2025-11-12 19:25:45 -0800

    Add a Program that Print all single-digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/**
+ * Author: Habila Makroghop Geoffrey;
+ * Program: WinMingle Community C Training
+ * Description: Just Learning about Numbers in C;
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) 
+{
+    char num = '0';
+
+    while  (num <= '9') 
+    {
+       putchar(num);
+       num ++;
+    }
+
+    putchar('\n');
+    return (0);
+}
+
+
