7ff1dc420c4fc01fc975e6ccf2283e759c99faa6
Habila Geoffrey <makrogh120@gmail.com>
2025-12-09 04:29:56 -0800

    This is my Header file for function_nested_loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);/* Prototype for _putchar */
+void print_alphabet(void); /* Prototype for print_alphabet */
+void print_alphabet_x10(void);/* Protype to Print-Alphabet ten times */
+int _islower(int c); /*This protype checks for lowercase character and return 1 if true or return 0 is otherwise*/
+int _isalpha(int c); /*This prototype checks for alphabetic characters.*/
+int print_sign(int n); /* This function that prints the sign of a number.*/
+int _abs(int n); /*This writes a function that computes the absolute value of an integer.*/
+int print_last_digit(int n); /*This prints the last digit of a number*/
+void jack_bauer(void);/* prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.*/
+void times_table(void); /* Prototype for the 9 times table, starting with 0. */
+int add(int, int); /* This Prototype adds two integers and returns the result */
+void print_to_98(int n);/*This Prototype prints all natural numbers from n to 98, followed by a new line. */
+void print_times_table(int n); /* This Prototype prints the multiplication table of n, starting from 0 × 0 up to n × n. */
+
+
+#endif 
+
