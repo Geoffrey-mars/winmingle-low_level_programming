90458182a6369844577fe6e6bdc4a630fe019462
Habila Geoffrey <makrogh120@gmail.com>
2026-01-09 06:23:36 -0800

    Updating the value pointed to by n to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * this function reset the value n in the address of n (&n)  *n = 402 - updates the value pointed to by n to 98
+ * @n: pointer to an integer
+ */
+
+void reset_to_98(int *n)
+
+{
+    *n = 98;
+
+}
