86795fbf23959eefe65c34716e60126f0768fca3
Habila Geoffrey <makrogh120@gmail.com>
2026-01-09 05:52:35 -0800

    testing the function positive_or_negative() using 0 as input

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests the function positive_or_negative with 0
+ * Return: 0
+ */
+int main(void)
+{
+    int i;
+
+    i = 0;
+    positive_or_negative(i);
+
+    return (0);
+}
