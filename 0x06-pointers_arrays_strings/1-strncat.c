0969025e7929879835c569e0bcb7437a16c6a4e3
Habila Geoffrey <makrogh120@gmail.com>
2026-01-17 05:37:49 -0800

    A function that concatenates two strings, using at most n bytes from src.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using at most n bytes from src
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of bytes to use from src
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    int i = 0;
+    int j = 0;
+
+    /* Find end of dest */
+    while (dest[i] != '\0')
+        i++;
+
+    /* Append src to dest, at most n characters */
+    while (src[j] != '\0' && j < n)
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    /* Adding final null terminator */
+    dest[i] = '\0';
+
+    return (dest);
+}
+
