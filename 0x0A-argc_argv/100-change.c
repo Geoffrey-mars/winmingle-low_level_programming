373a9869ce13a8b45cc35edfb30084e6eeabe5fa
Habila Geoffrey <makrogh120@gmail.com>
2026-01-20 05:13:28 -0800

    A program to calculate minimum coins for change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+    int cents;
+    int coins = 0;
+    int values[] = {25, 10, 5, 2, 1};
+    int i;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    cents = atoi(argv[1]);
+
+    if (cents < 0)
+    {
+        printf("0\n");
+        return (0);
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        coins += cents / values[i];
+        cents %= values[i];
+    }
+
+    printf("%d\n", coins);
+    return (0);
+}
+
