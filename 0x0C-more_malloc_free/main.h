2a12a9851bbe5b66dcf440f4a6bcc88f87abc2a8
Habila Geoffrey <makrogh120@gmail.com>
2026-01-20 06:01:23 -0800

    The Header file for this assignment

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+/* Task 0 */
+void *malloc_checked(unsigned int b);
+
+/* Task 1 */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/* Task 2 */
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/* Task 3 */
+int *array_range(int min, int max);
+
+/* Task 4 */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/* Task 5 */
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
