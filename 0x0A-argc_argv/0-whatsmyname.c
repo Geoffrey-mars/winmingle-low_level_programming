bfe03a051882b00e9038d34fc32ff89de1eb3020
Habila Geoffrey <makrogh120@gmail.com>
2026-01-20 05:03:51 -0800

    A program to print its own name using argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(int argc, char *argv[]) /* main receives terminal arguments */
+{
+    (void)argc;
+
+    printf("%s\n", argv[0]);
+    return (0);
+}
+
