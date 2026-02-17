22d3ca06a3ea4e2678c8c7e7b691bafc0cb1cd8e
Habila Geoffrey <makrogh120@gmail.com>
2026-02-17 06:19:14 -0800

    Add ABS function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? -(x) : (x)) /* creating a function like macro which change negative numbers to positive numbers  */
+
+
+#endif
