ef3c7b57dd010780b8b76d5c2bd68de60a98be1c
Habila Geoffrey <makrogh120@gmail.com>
2026-02-17 08:00:09 -0800

    Print opcode bytes of main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int n, i;
+    unsigned char *ptr;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
+
+    n = atoi(argv[1]);
+    if (n < 0)
+    {
+        printf("Error\n");
+        exit(2);
+    }
+
+    ptr = (unsigned char *)main;
+    for (i = 0; i < n; i++)
+    {
+        printf("%02x", ptr[i]);
+        if (i != n - 1)
+            printf(" ");
+    }
+    printf("\n");
+
+    return (0);
+}
+
