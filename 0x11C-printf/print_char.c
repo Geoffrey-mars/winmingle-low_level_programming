5004d53009da873a6c7ad6674e85bfa758cff2fb
Habila Geoffrey <makrogh120@gmail.com>
2026-02-17 08:12:21 -0800

    Add helper function to print single character

---
+++ 0x11C-printf/print_char.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_char - prints a single character
+ * @c: character to print
+ * Return: number of characters printed (always 1)
+ */
+int print_char(char c)
+{
+    return write(1, &c, 1);
+}
+
